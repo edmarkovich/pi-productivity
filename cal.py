60e82d2d97e14abe85c35c936892d43b182fb87b
Your Name <you@example.com>
2020-05-02 13:29:20 -0400

    ignore past events

--- cal.py
+++ cal.py
@@ -19,24 +19,21 @@ GPIO.setup(BUTOTN_PIN, GPIO.IN)
 def time_to_next_appt():
     #TODO: I think there's a python lib for this
     sp = subprocess.Popen("./pull_agenda", shell=True, stdout=subprocess.PIPE)
-    st = sp.stdout.read().strip().decode("ASCII")
-
-    if st == '':
-        print ("no apointment?")
-        return -1
     
-    #print(st)
+    while True:
+      print(st)
+      st = sp.stdout.readline().strip().decode("ASCII")
+      if not st: return -1
     
-    appt = datetime.datetime.strptime(st, '%a %b %d %I:%M%p')
-    #this won't work right near new years eve but who cares
-    appt = appt.replace(year=datetime.datetime.now().year)
+      appt = datetime.datetime.strptime(st, '%a %b %d %I:%M%p')
+      #this won't work right near new years eve but who cares
+      appt = appt.replace(year=datetime.datetime.now().year)
 
-    now = datetime.datetime.now()
-    diff = appt - now
+      now = datetime.datetime.now()
+      diff = appt - now
 
-    out= (diff.days*24 + diff.seconds/(60*60))
-    #print(out)
-    return(out)
+      out= (diff.days*24 + diff.seconds/(60*60))
+      if out > 0: return out
 
 GPIO.add_event_detect(BUTTON_PIN, GPIO.BOTH, bouncetime=300)        
 
