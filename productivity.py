bd02eed5452d3bee93d1adff6181e74f614c7edf
Your Name <you@example.com>
2020-05-20 19:01:40 -0400

    added error handle and retry

--- productivity.py
+++ productivity.py
@@ -44,12 +44,26 @@ def good_with_tasks(count):
  
 
 GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, bouncetime=5000)        
+error_count=0
 while True:
     last_poll = datetime.datetime.now()
     print("~~~~> ", last_poll)
     lights.all_on(True)
-
-    api_states = api_requests.get_api_states()
+    try:
+        api_states = api_requests.get_api_states()
+    except Exception as e:
+        error_count = error_count+1
+        print("Main Loop: something went wrong with API calls")
+        print(e)
+        delay = min(error_count*10,60*5) #retry fast, then back off up to 5min
+        print ("  Waiting for "+str(delay)+"sec before retrying")
+        for x in range(0,delay):
+            lights.all_on(True)
+            time.sleep(0.5)
+            lights.all_on(False)
+            time.sleep(0.5)
+        continue    
+        
     task_status = good_with_tasks(api_states['undone_tasks'])
     
 
